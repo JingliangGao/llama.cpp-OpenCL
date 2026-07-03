8032f6192e4c52a2afc227dc8c111c4f2657f924
JingliangGao <1121861747@qq.com>
2026-07-03 15:02:59 +0800

    [feat] update build script

--- build-for-debug.sh
+++ build-for-debug.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-PROJECT_DIR=$(dirname "$0")
-cd "$PROJECT_DIR"
+# set variables
+PROJECT_DIR=$(pwd)
+BACKEND="cpu"
+BUILD_FOLDER="build_${BACKEND}_profiler"
+
+
+# refresh build folder
+echo "[INFO] Refreshing build folder..."
+cd ${PROJECT_DIR}
+if [ -d ${BUILD_FOLDER} ]; then
+    rm -rf ${BUILD_FOLDER}
+fi
+mkdir ${BUILD_FOLDER}
+
+
+# configure project
+cd ${PROJECT_DIR}/${BUILD_FOLDER}
+echo "[INFO] Configuring project..."
+if [ ${BACKEND} == "cpu" ]; then
+    cmake .. \
+        -DGGML_CPU=ON \
+        -DLLAMA_GRAPH_PROFILER=ON
+elif [ ${BACKEND} == "cuda" ]; then
+    cmake .. \
+        -DGGML_CUDA=ON \
+        -DLLAMA_GRAPH_PROFILER=ON \
+        -DCMAKE_CUDA_HOST_COMPILER=/usr/bin/gcc-10 \
+        -DCMAKE_C_COMPILER=/usr/bin/gcc-10 \
+        -DCMAKE_CXX_COMPILER=/usr/bin/g++-10
+fi
+
+# build project
+echo "[INFO] Building project..."
+make -j16
+
+
+echo "[INFO] All done."
+
+
 
 echo ">> [INFO] All finished."
